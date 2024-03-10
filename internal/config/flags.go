f3bf46f4363d244fc8ed7262271ce0504fe96993
ajugalushkin <ajugalushkin@gmail.com>
2024-03-10 23:04:59 +0300

    Some fix for PullRequest

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -17,7 +17,6 @@ func ParseFlags() {
 	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
 		BaseURL = envBaseURL
 	}
-	
 	flag.StringVar(&FlagRunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.StringVar(&BaseURL, "b", "http://localhost:8080", "Base URL for POST request")
 	flag.Parse()
