a0ab5fd8e1eec355dc29b4c3cdf36039c8bdc5f6
ajugalushkin <50012581+ajugalushkin@users.noreply.github.com>
2024-03-12 09:42:15 +0300

    Revert "Complete iter5"

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -1,9 +1,6 @@
 package config
 
-import (
-	"flag"
-	"os"
-)
+import "flag"
 
 var (
 	FlagRunAddr string
@@ -11,13 +8,6 @@ var (
 )
 
 func ParseFlags() {
-	if envRunAddr := os.Getenv("RUN_ADDR"); envRunAddr != "" {
-		FlagRunAddr = envRunAddr
-	}
-	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
-		BaseURL = envBaseURL
-	}
-	
 	flag.StringVar(&FlagRunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.StringVar(&BaseURL, "b", "http://localhost:8080", "Base URL for POST request")
 	flag.Parse()
